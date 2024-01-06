c4c1c4e9167000565f0fd4e311011ce5ab190eb5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-06 22:25:55 +0200

    feat: Adjust app to be mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
     <div
       className={`${
         openNav ? 'overflow-y-hidden' : 'overflow-y-visible'
-      } overflow-x-hidden w-full h-screen relative flex flex-col`}
+      } overflow-x-hidden w-full h-screen relative flex flex-col gap-12`}
     >
       <Navbar openNav={openNav} setOpenNav={setOpenNav} />
       <About />
-      {/* <Projects />  */}
+      <Projects />
     </div>
   );
 }
