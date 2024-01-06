5393cc2282a73cadd74ed1166ed0b69777510414
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-06 22:00:43 +0200

    feat: Add mobile nav icon

---
+++ src/components/MenuIcon/MenuIcon.tsx
@@ -0,0 +1,14 @@
+import './style.css';
+const MenuIcon = () => {
+  return (
+    <input
+      type='checkbox'
+      role='button'
+      aria-label='Display the menu'
+      className='menu'
+
+    />
+  );
+};
+
+export default MenuIcon;
