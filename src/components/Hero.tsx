de048f23c4513f628f12d5b93fc6c569e785a183
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-07 17:13:08 +0200

    style: update width of hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
 const Hero = () => {
   return (
-    <div className='flex w-full flex-col gap-4 lg:flex-row px-4'>
+    <div className='flex w-full flex-col gap-4 items-center justify-center lg:flex-row px-4 max-w-7xl'>
       {/* Hero text container*/}
-      <div className='w-full flex flex-col gap-2 justify-center items-start mt-8'>
-        <h1 className='text-5xl text-seashellPeach-50 uppercase'>Greetings!</h1>
-        <p className='text-karry-200 font-light text-base ml-1'>
+      <div className='w-full flex flex-col gap-2 justify-center items-start text-left mt-8'>
+        <h1 className='text-5xl text-seashellPeach-50 uppercase max-w-3xl lg:max-w-xl w-full'>
+          Greetings!
+        </h1>
+        <p className='text-karry-200 font-light text-base ml-1  max-w-3xl lg:max-w-xl'>
           I'm not your average code monkey. I'm an alchemist, transforming lines
           of text into pixelated masterpieces. With a dash of HTML, a sprinkle
           of CSS, and a generous helping of Next.js, I sculpt interfaces that
@@ -14,14 +16,14 @@ const Hero = () => {
           unforgettable. Websites that make you say "wow" before you even hit
           enter.
         </p>
-        <p className='text-lavender-300 text-base font-light italic mt-4 ml-1 line-through'>
+        <p className='text-lavender-300 text-base font-light italic mt-4 ml-1 line-through max-w-3xl lg:max-w-xl w-full'>
           This bio was crafted with a sprinkle of artificial intelligence.
         </p>
       </div>
       {/* Avatar */}
       <img
         src='avatar.png'
-        className='rounded-full bg-flesh-200 w-72 h-72 self-center'
+        className='rounded-full bg-flesh-200 size-72 self-center md:size-96'
       />
     </div>
   );
