e280cf6e15a1b5e0a7465e800fea464b919d22e3
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-07 17:12:19 +0200

    style: change margins and gap

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,8 +8,8 @@ const Projects = () => {
   };
 
   return (
-    <div className='flex flex-col gap-2 w-full p-4 text-seashellPeach-50 items-center justify-center mt-12 lg:mt-48'>
-      <h1 className='max-w-[570px] lg:max-w-full text-3xl uppercase text-left w-full xl:max-w-7xl ml-2'>
+    <div className='flex flex-col gap-2 w-full p-4 text-seashellPeach-50 items-center justify-center gap-4'>
+      <h1 className='max-w-[570px] lg:max-w-full text-3xl uppercase text-left w-full xl:max-w-7xl xl:text-5xl ml-2'>
         Projects
       </h1>
       {/* Project container */}
