e1d9ee0c9d6691011ef23ba9131cdd93ae56f201
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-07 16:08:03 +0200

    style: adjust hero section for tablets and desktop

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
-    <div className='flex w-full flex-col gap-4'>
+    <div className='flex w-full flex-col gap-4 lg:flex-row px-4'>
       {/* Hero text container*/}
-      <div className='w-full p-4 flex flex-col gap-2 justify-center items-start mt-8'>
+      <div className='w-full flex flex-col gap-2 justify-center items-start mt-8'>
         <h1 className='text-5xl text-seashellPeach-50 uppercase'>Greetings!</h1>
         <p className='text-karry-200 font-light text-base ml-1'>
           I'm not your average code monkey. I'm an alchemist, transforming lines
