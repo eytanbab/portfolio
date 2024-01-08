f5e56d6490813f86027d59e185aeb504ee6d8f7b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-08 18:17:55 +0200

    style: Imporve flex gap between components

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,14 @@ import Projects from './components/Projects';
 
 function App() {
   const [openNav, setOpenNav] = useState<boolean>(false);
-
   return (
     <div
       className={`${
         openNav ? 'overflow-y-hidden' : 'overflow-y-visible'
-      } overflow-x-hidden w-screen h-screen relative flex flex-col gap-48 items-center`}
+      } overflow-x-hidden w-screen h-screen relative flex flex-col gap-4 items-center`}
     >
       <Navbar openNav={openNav} setOpenNav={setOpenNav} />
-      <div className='w-full max-w-7xl flex flex-col justify-center items-center'>
+      <div className='w-full max-w-7xl flex flex-col justify-center gap-24 items-center'>
         <About />
         <Projects />
       </div>
