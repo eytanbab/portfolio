1cba93adb02b9058c4cd52c52f839b9040e56e2c
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-08 18:21:16 +0200

    refactor: Change navbar position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ const Navbar = ({ openNav, setOpenNav }: NavbarProps) => {
       </div>
 
       {/* Desktop view*/}
-      <nav className='hidden lg:flex absolute top-0 left-0  p-4 h-20 w-full bg-woodsmoke-950/10 backdrop-blur-3xl text-seashellPeach-50  justify-between items-center'>
+      <nav className='hidden lg:flex p-4 h-20 w-full bg-woodsmoke-950/10 backdrop-blur-3xl text-seashellPeach-50 justify-between items-center'>
         <h1 className='text-xl text-flesh-200 font-light'>Ethan Babaev</h1>
         <ul className='flex gap-8 items-center'>
           <li>
