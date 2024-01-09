0f5ecd3b36258679bec508a415464a90399ddbe1
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-09 20:22:17 +0200

    feat: Replace placeholder to real data

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react';
 import Project from './Project';
+import { data } from './data';
+
 const Projects = () => {
   return (
-    <div className='flex flex-col w-full p-4 text-seashellPeach-50 items-center justify-start gap-4 min-h-screen'>
-      <h1 className='max-w-[570px] lg:max-w-full text-3xl uppercase text-left w-full xl:text-5xl ml-2'>
+    <div className='flex flex-col w-full p-4 text-seashellPeach-50 items-center justify-start gap-2 min-h-screen'>
+      <h1 className='max-w-[570px] lg:max-w-full text-3xl uppercase text-left w-full xl:text-5xl'>
         Projects
       </h1>
-      <Project />
+      <div className='flex flex-col gap-8'>
+        {data?.map((project) => (
+          <Project key={project.projectId} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
