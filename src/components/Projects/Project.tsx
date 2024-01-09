b2c454306c3b6c94deeda6371932399333f22ba7
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-09 20:21:39 +0200

    feat: Add project information

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,31 +1,37 @@
 import { ChevronDown } from 'lucide-react';
 import { useState } from 'react';
+import { projectProps } from './data';
 
-const Project = () => {
+const Project = ({ project }: { project: projectProps }) => {
   const [expand, setExpand] = useState(false);
   const handleExpand = () => {
     setExpand((prev) => !prev);
   };
   return (
-    <div className='w-full flex flex-col items-center lg:flex-row lg:gap-4 xl:justify-center'>
+    <div
+      className={`${
+        project.projectId % 2 === 0 ? 'lg:flex-row-reverse' : ''
+      } w-full flex flex-col items-center lg:flex-row lg:gap-4 xl:justify-center`}
+    >
       {/* Site image */}
-
       <img
-        src='defino.jpg'
-        className='rounded-t-xl object-contain max-h-80 lg:max-h-96 lg:rounded-xl 3xl:max-h-[720px]'
+        src={project.projectImagePath}
+        className='rounded-t-xl object-contain max-h-80 xl:max-h-96 lg:rounded-xl 3xl:max-h-[720px] '
       />
       {/* Project description */}
-      <div className='lg:flex w-full bg-shark-950 rounded-b-xl flex flex-col p-2 lg:h-96 max-w-[570px] lg:max-h-96 lg:max-w-xl 3xl:max-h-[720px] 3xl:h-full lg:rounded-xl'>
-        <div className='w-full flex justify-between items-center'>
+      <div className='lg:flex w-full bg-shark-950 rounded-b-xl flex flex-col p-4 lg:h-80 max-w-[570px] xl:max-h-96 xl:h-full xl:max-w-xl 3xl:max-h-[720px] 3xl:h-full lg:rounded-xl'>
+        <div
+          className='w-full flex justify-between items-center '
+          onClick={handleExpand}
+        >
           <h1
             className={`${
               expand ? 'text-3xl' : 'text-xl'
             }  font-light uppercase`}
           >
-            Defino
+            {project.projectName}
           </h1>
           <ChevronDown
-            onClick={handleExpand}
             className={`${
               expand ? 'rotate-180' : 'rotate-0'
             } transition-transform duration-300 lg:hidden`}
@@ -37,30 +43,27 @@ const Project = () => {
           } lg:flex lg:flex-col  lg:h-full`}
         >
           <p className='font-extralight text-karry-200 lg:text-sm'>
-            Unleash the power of language with Defino, the ultimate online
-            dictionary. Explore over 750,000 words with ease, find precise
-            definitions, and discover unexpected connections. Synonyms,
-            antonyms, and etymology are just a click away, making Defino your
-            perfect companion for writing, learning, or simply expanding your
-            vocabulary.
+            {project.projectDesc}
           </p>
-          <p className='italic text-lavender-300 mt-4 font-light text-sm'>
+          <p className='italic text-lavender-300 mt-4 font-light text-xs'>
             totally not written by chatGPT
           </p>
           {/* Links */}
-          <ul className='flex gap-4 self-center flex-1 my-8  h-full'>
-            <li className='underline font-extralight'>Link</li>
-            <li className='underline font-extralight'>Github</li>
+          <ul className='flex gap-4 self-center my-8'>
+            <a href={project.link} className='underline font-extralight'>
+              Link
+            </a>
+            <a href={project.repo} className='underline font-extralight'>
+              Github
+            </a>
           </ul>
           {/* Used technology*/}
-          <ul className='flex self-center gap-4 items-center text-center text-xs uppercase'>
-            <li className='px-4 py-1  rounded-full bg-brightGray-900'>React</li>
-            <li className='px-4 py-1 rounded-full bg-brightGray-900'>
-              Tailwind CSS
-            </li>
-            <li className='px-4 py-1 rounded-full bg-brightGray-900'>
-              Dictionary API
-            </li>
+          <ul className='flex self-center gap-4 items-center h-full text-center text-xs uppercase lg:items-end'>
+            {project.techUsed.map((tech) => (
+              <li className='px-4 py-1  rounded-full bg-brightGray-900'>
+                {tech}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
