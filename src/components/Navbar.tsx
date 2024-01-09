c1d2f2ba672de16fd83ed1ba87b0d9be37b06de3
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-09 17:08:23 +0200

    refactor: Remove unused code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,27 +36,6 @@ const Navbar = ({ openNav, setOpenNav }: NavbarProps) => {
         </ul>
       </div>
 
-      {/* Desktop view*/}
-      {/* <nav className='hidden lg:flex p-4 h-20 w-full bg-woodsmoke-950/10 backdrop-blur-3xl text-seashellPeach-50 justify-between items-center'>
-        <h1 className='text-xl text-flesh-200 font-light'>Ethan Babaev</h1>
-        <ul className='flex gap-8 items-center'>
-          <li>
-            <span className='text-flesh-200 font-normal'>01. </span>
-            About
-          </li>
-          <li>
-            <span className='text-flesh-200 font-normal'>02. </span>
-            Projects
-          </li>
-          <li>
-            <span className='text-flesh-200 font-normal'>03. </span>
-            Contact
-          </li>
-          <li className='border-[1.5px] border-flesh-200 px-6 py-1 rounded-full'>
-            Resume
-          </li>
-        </ul>
-      </nav> */}
       <nav className='hidden lg:flex fill-seashellPeach-50 bg-seashellPeach-50/10 backdrop-blur-3xl fixed right-8 top-[50%] z-50 flex-col gap-4 p-2 rounded-full translate-y-[-50%]'>
         {/* About icon */}
         <svg
