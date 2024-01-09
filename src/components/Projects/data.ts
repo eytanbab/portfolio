e7e200f2235f4002fd9c5f7062d5f5b113faf55b
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-09 20:21:04 +0200

    feat: Add tech used

--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -1,11 +1,21 @@
-export const data = [
+export type projectProps = {
+  projectId: number;
+  projectName: string;
+  projectDesc: string;
+  techUsed: string[];
+  projectImagePath: string;
+  link: string;
+  repo: string;
+};
+
+export const data: projectProps[] = [
   {
     projectId: 1,
     projectName: 'Advice Generator',
     projectDesc:
       'Get instant guidance with our advice generator app! Simply click the button and receive a random piece of advice, based on expert insights and proven psychological principles.',
-    techUsed: [],
-    projectImageUrl: 'https://i.ibb.co/5rV5yBt/advice-generator.png',
+    techUsed: ['React', 'Tailwind CSS', 'Advice Slip API'],
+    projectImagePath: '/projectImages/advice-generator.png',
     link: 'https://advice-generator-7.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/advice-generator',
   },
@@ -14,8 +24,8 @@ export const data = [
     projectName: 'Dictionary',
     projectDesc:
       'Find word meanings quickly with our easy-to-use dictionary web app. Access millions of words and definitions, stay up-to-date with the latest language trends, and start exploring now!',
-    techUsed: [],
-    projectImageUrl: 'https://i.ibb.co/9gsPMzX/dictionary.png',
+    techUsed: ['React', 'Tailwind CSS', 'Dictionary API'],
+    projectImagePath: 'https://i.ibb.co/9gsPMzX/dictionary.png',
     link: 'https://dict-web-app.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/dictionary-web-app',
   },
@@ -25,41 +35,39 @@ export const data = [
   //   projectDesc:
   //     'Explore the wonders of the cosmos from the comfort of your own home, with our interactive guides and immersive experiences. Join us on this epic adventure and start your space journey today!',
   //   techUsed: [],
-  //   projectImageUrl:
+  //   projectImagePath:
   //     'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
   //   link: '',
   //   repo: 'https://github.com/eytanbab/react/tree/master/space-tourism-web-app',
   // },
   {
-    projectId: 4,
+    projectId: 3,
     projectName: 'Tip calculator',
     projectDesc:
       'Tired of the hassle of calculating tips? Our tip calculator app is here to help! With just a few taps, you can easily split the bill and calculate the perfect tip for your meal. ',
-    techUsed: [],
-    projectImageUrl: 'https://i.ibb.co/cDWJDw1/tip-calculator.png',
+    techUsed: ['React', 'CSS'],
+    projectImagePath: 'https://i.ibb.co/cDWJDw1/tip-calculator.png',
     link: 'https://calculate-my-tip.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/tip-calculator-app',
   },
   {
-    projectId: 5,
+    projectId: 4,
     projectName: 'Shortly',
     projectDesc:
       'Introducing our URL shortener - the perfect tool for simplifying and streamlining your online presence! With our app, you can quickly and easily generate shortened links that are easy to remember and share.',
-    techUsed: [],
-    projectImageUrl: 'https://i.ibb.co/7nCBj0H/shortly.png',
+    techUsed: ['React', 'Tailwind CSS', 'shrtco.de API'],
+    projectImagePath: 'https://i.ibb.co/7nCBj0H/shortly.png',
     link: 'https://url-shortener-2.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/url-shortener-2',
   },
   {
-    projectId: 6,
+    projectId: 5,
     projectName: 'Weatherly',
     projectDesc:
       "Stay ahead of the weather with our user-friendly web app! With real-time updates and accurate forecasts, you'll always be prepared for whatever Mother Nature has in store. ",
-    techUsed: [],
-    projectImageUrl: 'https://i.ibb.co/vzCx0yF/weatherly.png',
+    techUsed: ['React', 'Tailwind CSS', 'weatherapi '],
+    projectImagePath: 'https://i.ibb.co/vzCx0yF/weatherly.png',
     link: 'https://weatherly-web-app.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/weather-web-app',
   },
 ];
-
-export default data;
