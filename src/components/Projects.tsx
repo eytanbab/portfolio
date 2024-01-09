ddddee810e37db7c82b68bd769c8e7a6587783e2
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-09 03:24:42 +0200

    refactor: Remove unnecessary code

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,7 +23,7 @@ const Projects = () => {
 
         {/* Mobile & Tablet view */}
         {/* Project description */}
-        <div className='lg:hidden w-full bg-shark-950 rounded-b-xl flex flex-col  p-2 h-max max-w-[570px] '>
+        <div className='lg:flex w-full bg-shark-950 rounded-b-xl flex flex-col p-2 lg:h-96 max-w-[570px] lg:max-h-96 lg:max-w-xl 3xl:max-h-[720px] 3xl:h-full lg:rounded-xl'>
           <div className='w-full flex justify-between items-center'>
             <h1
               className={`${
@@ -36,11 +36,15 @@ const Projects = () => {
               onClick={handleExpand}
               className={`${
                 expand ? 'rotate-180' : 'rotate-0'
-              } transition-transform duration-300`}
+              } transition-transform duration-300 lg:hidden`}
             />
           </div>
-          <div className={`${expand ? 'flex flex-col' : 'hidden'}`}>
-            <p className='font-extralight text-karry-200'>
+          <div
+            className={`${
+              expand ? 'flex flex-col' : 'hidden'
+            } lg:flex lg:flex-col  lg:h-full`}
+          >
+            <p className='font-extralight text-karry-200 lg:text-sm'>
               Unleash the power of language with Defino, the ultimate online
               dictionary. Explore over 750,000 words with ease, find precise
               definitions, and discover unexpected connections. Synonyms,
@@ -52,43 +56,7 @@ const Projects = () => {
               totally not written by chatGPT
             </p>
             {/* Links */}
-            <ul className='flex gap-4 self-center flex-1 my-8'>
-              <li className='underline font-extralight'>Link</li>
-              <li className='underline font-extralight'>Github</li>
-            </ul>
-            {/* Used technology*/}
-            <ul className='flex self-center gap-4 items-center text-center text-xs uppercase'>
-              <li className='px-4 py-1  rounded-full bg-brightGray-900'>
-                React
-              </li>
-              <li className='px-4 py-1 rounded-full bg-brightGray-900'>
-                Tailwind CSS
-              </li>
-              <li className='px-4 py-1 rounded-full bg-brightGray-900'>
-                Dictionary API
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        {/* Desktop view*/}
-        <div className='hidden lg:flex w-full bg-shark-950 rounded-xl flex-col p-4 h-96 max-w-xl 3xl:max-h-[720px] 3xl:h-full'>
-          <h1 className='text-3xl font-light uppercase'>Defino</h1>
-          {/* Description */}
-          <div className='flex flex-col h-full'>
-            <p className='font-extralight text-sm  text-karry-200'>
-              Unleash the power of language with Defino, the ultimate online
-              dictionary. Explore over 750,000 words with ease, find precise
-              definitions, and discover unexpected connections. Synonyms,
-              antonyms, and etymology are just a click away, making Defino your
-              perfect companion for writing, learning, or simply expanding your
-              vocabulary.
-            </p>
-            <p className='italic text-lavender-300 mt-2 font-light text-sm'>
-              totally not written by chatGPT
-            </p>
-            {/* Links */}
-            <ul className='flex gap-4 self-center flex-1 my-4'>
+            <ul className='flex gap-4 self-center flex-1 my-8  h-full'>
               <li className='underline font-extralight'>Link</li>
               <li className='underline font-extralight'>Github</li>
             </ul>
