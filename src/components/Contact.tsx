24336fb659e80dcfd94c0a54042c7ae52973823c
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-10 18:47:20 +0200

    feat: Add resume button

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,14 +6,17 @@ const Contact = () => {
   return (
     <div className='w-full  text-seashellPeach-50 flex gap-8 items-center mt-4 justify-center lg:justify-start lg:ml-1'>
       <a href='https://github.com/eytanbab'>
-        <Github size={24} />
+        <Github className='size-8' />
       </a>
       <a href='https://www.linkedin.com/in/ethan-babaev/'>
-        <Linkedin size={24} />
+        <Linkedin className='size-8' />
       </a>
       <a href='mailto:ethan.babaev@gmail.com'>
-        <Mail size={24} />
+        <Mail className='size-8' />
       </a>
+      <button className='px-4 py-1 border-2 border-seashellPeach-50 rounded-full hover:text-seashellPeach-50/85 hover:border-seashellPeach-50/85'>
+        Resume
+      </button>
     </div>
   );
 };
