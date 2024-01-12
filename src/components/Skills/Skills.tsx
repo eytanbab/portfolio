656dbe2d9936bb79076ba706f64ddd1792989732
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-12 17:01:48 +0200

    style: update background color of marquee text

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -11,25 +11,31 @@ const Skills = ({
       ref={skillsRef}
       className='text-5xl uppercase min-h-screen snap-start font-black gap-4 text-center overflow-hidden relative lg:text-9xl flex flex-col items-center justify-center w-full skew-y-2'
     >
-      <Marquee direction='right' className='text-seashellPeach-50'>
+      <Marquee
+        direction='right'
+        className='text-seashellPeach-50 bg-shark-950/50'
+      >
         react
       </Marquee>
       <Marquee direction='left' className='text-lavender-300'>
         javascript
       </Marquee>
-      <Marquee direction='right' className='text-hoki-500'>
+      <Marquee direction='right' className='text-hoki-500 bg-lavender-300/10'>
         typescript
       </Marquee>
       <Marquee direction='left' className='text-flesh-200'>
         next.js
       </Marquee>
-      <Marquee direction='right' className='text-shark-950'>
+      <Marquee direction='right' className='text-shark-950 bg-lavender-300/10'>
         postgreSQL
       </Marquee>
-      <Marquee direction='left' className='text-karry-200'>
+      <Marquee direction='left' className='text-karry-200 '>
         node.js
       </Marquee>
-      <Marquee direction='right' className='text-brightGray-900'>
+      <Marquee
+        direction='right'
+        className='text-brightGray-900 bg-shark-950/50'
+      >
         python
       </Marquee>
     </div>
