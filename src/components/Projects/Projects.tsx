74083c64c28d9f081ba8e2bb62ca29ca95c089ee
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-13 19:43:34 +0200

    feat: Remove h1 title from projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -12,9 +12,8 @@ const Projects = ({
       className='flex flex-col p-4 text-seashellPeach-50 items-center justify-center gap-4 min-h-screen snap-start lg:w-full'
     >
       <div className='grid grid-cols-1 lg:grid-cols-2 place-items-between gap-4'>
-        <h1 className='lg:max-w-full text-3xl uppercase text-left self-start col-span-full xl:text-5xl font-extrabold'>
-          Projects
-        </h1>
+        
+
         {data?.map((project) => (
           <Project key={project.projectId} project={project} />
         ))}
