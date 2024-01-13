e7a8040017fccd81d2b8e9df978fd9e2cb861773
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-01-13 19:38:14 +0200

    refactor: Change projects order

--- src/components/Projects/data.ts
+++ src/components/Projects/data.ts
@@ -11,6 +11,16 @@ export type projectProps = {
 export const data: projectProps[] = [
   {
     projectId: 1,
+    projectName: 'Defino',
+    projectDesc:
+      'Find word meanings quickly with our easy-to-use dictionary web app. Access millions of words and definitions, stay up-to-date with the latest language trends, and start exploring now!',
+    techUsed: ['React', 'Tailwind CSS', 'Dictionary API'],
+    projectImagePath: '/mobile_images/defino_mobile.png',
+    link: 'https://dict-web-app.netlify.app/',
+    repo: 'https://github.com/eytanbab/react/tree/master/dictionary-web-app',
+  },
+  {
+    projectId: 2,
     projectName: 'Advice Generator',
     projectDesc:
       'Get instant guidance with our advice generator app! Simply click the button and receive a random piece of advice, based on expert insights and proven psychological principles.',
@@ -20,38 +30,7 @@ export const data: projectProps[] = [
     repo: 'https://github.com/eytanbab/react/tree/master/advice-generator',
   },
   {
-    projectId: 2,
-    projectName: 'Defino',
-    projectDesc:
-      'Find word meanings quickly with our easy-to-use dictionary web app. Access millions of words and definitions, stay up-to-date with the latest language trends, and start exploring now!',
-    techUsed: ['React', 'Tailwind CSS', 'Dictionary API'],
-    projectImagePath: '/mobile_images/defino_mobile.png',
-    link: 'https://dict-web-app.netlify.app/',
-    repo: 'https://github.com/eytanbab/react/tree/master/dictionary-web-app',
-  },
-  // {
-  //   projectId: 3,
-  //   projectName: 'Space Tourism',
-  //   projectDesc:
-  //     'Explore the wonders of the cosmos from the comfort of your own home, with our interactive guides and immersive experiences. Join us on this epic adventure and start your space journey today!',
-  //   techUsed: [],
-  //   projectImagePath:
-  //     'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
-  //   link: '',
-  //   repo: 'https://github.com/eytanbab/react/tree/master/space-tourism-web-app',
-  // },
-  {
     projectId: 3,
-    projectName: 'Tip calculator',
-    projectDesc:
-      'Tired of the hassle of calculating tips? Our tip calculator app is here to help! With just a few taps, you can easily split the bill and calculate the perfect tip for your meal. ',
-    techUsed: ['React', 'CSS'],
-    projectImagePath: '/mobile_images/calculator_mobile.png',
-    link: 'https://calculate-my-tip.netlify.app/',
-    repo: 'https://github.com/eytanbab/react/tree/master/tip-calculator-app',
-  },
-  {
-    projectId: 4,
     projectName: 'Shortly',
     projectDesc:
       'Introducing our URL shortener - the perfect tool for simplifying and streamlining your online presence! With our app, you can quickly and easily generate shortened links that are easy to remember and share.',
@@ -61,7 +40,7 @@ export const data: projectProps[] = [
     repo: 'https://github.com/eytanbab/react/tree/master/url-shortener-2',
   },
   {
-    projectId: 5,
+    projectId: 4,
     projectName: 'Weatherly',
     projectDesc:
       "Stay ahead of the weather with our user-friendly web app! With real-time updates and accurate forecasts, you'll always be prepared for whatever Mother Nature has in store. ",
@@ -69,5 +48,15 @@ export const data: projectProps[] = [
     projectImagePath: '/mobile_images/weatherly_mobile.png',
     link: 'https://weatherly-web-app.netlify.app/',
     repo: 'https://github.com/eytanbab/react/tree/master/weather-web-app',
+  },
+  {
+    projectId: 5,
+    projectName: 'Tip calculator',
+    projectDesc:
+      'Tired of the hassle of calculating tips? Our tip calculator app is here to help! With just a few taps, you can easily split the bill and calculate the perfect tip for your meal. ',
+    techUsed: ['React', 'CSS'],
+    projectImagePath: '/mobile_images/calculator_mobile.png',
+    link: 'https://calculate-my-tip.netlify.app/',
+    repo: 'https://github.com/eytanbab/react/tree/master/tip-calculator-app',
   },
 ];
