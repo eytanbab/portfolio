29a445346389b5350474dcac26df792e794a9fb5
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-02-16 03:23:45 +0200

    refactor: Remove blank spaces

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -12,8 +12,6 @@ const Projects = ({
       className='flex flex-col p-4 text-seashellPeach-50 items-center justify-center gap-4 min-h-screen snap-start lg:w-full'
     >
       <div className='grid grid-cols-1 lg:grid-cols-2 place-items-between gap-4'>
-        
-
         {data?.map((project) => (
           <Project key={project.projectId} project={project} />
         ))}
