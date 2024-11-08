f4d601b3e3e688e9186eab6a2919b00c76c37a17
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-11-08 18:22:31 +0200

    refactor: remove loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects/Projects';
 import RecommendationCards from './components/RecommendationCards';
-import Loader from './components/Loader/Loader';
-import { AnimatePresence, motion, useScroll } from 'framer-motion';
+// import Loader from './components/Loader/Loader';
 import Skills from './components/Skills/Skills';
+import { motion, useScroll } from 'framer-motion';
 
 function App() {
-  const [loading, isLoading] = useState<boolean>(true);
+  // const [loading, isLoading] = useState<boolean>(true);
   const aboutRef: React.MutableRefObject<HTMLDivElement | null> | null =
     useRef(null);
   const recommendationRef: React.MutableRefObject<HTMLDivElement | null> | null =
@@ -21,35 +21,35 @@ function App() {
 
   return (
     <div className='overflow-x-hidden w-full relative flex flex-col items-center overflow-hidden'>
-      <AnimatePresence>
+      {/* <AnimatePresence>
         {loading && <Loader isLoading={isLoading} />}
-      </AnimatePresence>
-      {!loading && (
+      </AnimatePresence> */}
+      {/* {!loading && ( */}
+      <motion.div
+        initial={{ opacity: 0, y: 200 }} // Start hidden and slightly below
+        animate={{ opacity: 1, y: 0 }} // Fade in and move up to original position
+        transition={{ duration: 1, type: 'tween' }}
+        className=' w-full flex flex-col justify-center items-center'
+      >
+        <Navbar
+          aboutRef={aboutRef}
+          recommendationRef={recommendationRef}
+          projectsRef={projectsRef}
+          skillsRef={skillsRef}
+        />
+        {/* Progress line */}
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, type: 'tween' }}
-          className=' w-full flex flex-col justify-center items-center'
-        >
-          <Navbar
-            aboutRef={aboutRef}
-            recommendationRef={recommendationRef}
-            projectsRef={projectsRef}
-            skillsRef={skillsRef}
-          />
-          {/* Progress line */}
-          <motion.div
-            className='bg-lavender-300 fixed w-px top-0 left-8 bottom-0 origin-top-left shadow-glow'
-            style={{ scaleY: scrollYProgress }}
-          />
-          <div className='w-screen max-w-7xl flex flex-col justify-center gap-24 items-center snap-y  relative oveflow-x-hidden'>
-            <About aboutRef={aboutRef} />
-            <RecommendationCards recommendationRef={recommendationRef} />
-            <Skills skillsRef={skillsRef} />
-            <Projects projectsRef={projectsRef} />
-          </div>
-        </motion.div>
-      )}
+          className='bg-lavender-300 fixed w-px top-0 left-8 bottom-0 origin-top-left shadow-glow'
+          style={{ scaleY: scrollYProgress }}
+        />
+        <div className='w-screen max-w-7xl flex flex-col justify-center gap-24 items-center snap-y  relative oveflow-x-hidden'>
+          <About aboutRef={aboutRef} />
+          <RecommendationCards recommendationRef={recommendationRef} />
+          <Skills skillsRef={skillsRef} />
+          <Projects projectsRef={projectsRef} />
+        </div>
+      </motion.div>
+      {/* )} */}
     </div>
   );
 }
